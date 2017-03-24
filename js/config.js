b711733d3635b5640f0c27780fd92ca2b2964033
Clark Weckmann <cweckmaniii@gmail.com>
2017-03-24 05:14:48 -0500

    update

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+    "m778718799-63a4d9fbc7a136b6383e8733",  //APPS
+    "m778718805-00b39f74211e63f213133581", //LAB
+    "m778718413-36238b805dc5095c751511cc", //ClarkHacks.com
+    "m778718795-87025b5f14281a0112c30117", //CDN
+    "m778802616-b2c2d91dcab6353f63fdf545", //DEV
+    "m778725396-a82eb2e429daa1fa81e26b31", // Github XXX.153 Host 1
+    "m778725399-beff1a78a86f4edead179679", // Github XXX.154 Host 2
+    "m778725626-7327f3cd7deb4ebccb4e427f", //sc.clarkhacks.com
+    "m778802590-d83d6ab42ae05b65854c5d55"  // XXX.clarkhacks.com
+];
+// refresh interval (in seconds)
+var __refresh = 900;
